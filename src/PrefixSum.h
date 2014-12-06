9f5a5c9f948c5cbfa3797c71f9f4b0ef4d93ffa9
pamelamiletic <pamelamiletic2@gmail.com>
2014-12-06 20:23:01 +0100

    Added file for PrefixSum

---
+++ src/PrefixSum.h
@@ -0,0 +1,14 @@
+#ifndef PrefixSum_H
+#define PrefixSum_H
+
+#include <stdio.h>
+#include <string>
+#include <conio.h>
+#include <iostream>
+
+class PrefixSum{
+public:
+	int count(std::string word, char alpha);
+};
+
+#endif
