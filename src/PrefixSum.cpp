a01a3efdfb866f89765a0c5bb228bda3113d3b0e
pamelamiletic <pamelamiletic2@gmail.com>
2014-12-06 18:45:39 +0100

    PrefixSum.cpp
    Input a word

---
+++ src/PrefixSum.cpp
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <string>
+#include <iostream>
+
+int main()
+{
+	char *arr;
+	//prompting user to input word
+	std::string word;
+	std::cout << "ENTER THE WORD \n";
+	std::cin >> word;
+
+	int n = word.length();
+	arr = new char[n];
+	for (unsigned i = 0; i<word.length(); ++i)
+	{
+		char alpha = word.at(i);
+		arr[i] = alpha;
+		std::cout << alpha << std::endl;
+	}
+}
