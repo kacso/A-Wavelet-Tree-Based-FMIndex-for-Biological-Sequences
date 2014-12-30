a7ccdbc406eab3ced1e899011ac5518bfd722325
pamelamiletic <pamelamiletic2@gmail.com>
2014-12-31 00:05:57 +0100

    Added methods from WaveletTree
    Added methods from WaveletTree.
    Creating object PrefixSum in constructor

--- src/LFTable.h
+++ src/LFTable.h
@@ -5,20 +5,28 @@
 #include <string>
 #include <conio.h>
 #include <iostream>
+#include "IOcc.h"
+#include "PrefixSum.h"
 #include "WaveletTree.h"
 
 class LFTable{
 private:
-	std::string textFirst;
-	std::string textLast;
-	WaveletTree *treeFirst;
-	WaveletTree *treeLast;
+	IOcc *treeFirst;
+	IOcc *treeLast;
+	PrefixSum *prefixSumFirst;
+	PrefixSum *prefixSumLast;
 public:
 	LFTable(std::string word);
 	int countFirst(char sign);
 	int countLast(char sign);
 	int getRankFirst(char sign, int index);
 	int getRankLast(char sign, int index);
+	char LFTable::getCharFirst(unsigned index);
+	char LFTable::getCharLast(unsigned index);
+	unsigned LFTable::getIndexFirst(char character, unsigned rank);
+	unsigned LFTable::getIndexLast(char character, unsigned rank);
+	unsigned LFTable::getLengthFirst();
+	unsigned LFTable::getLengthLast();
 };
 
 #endif
