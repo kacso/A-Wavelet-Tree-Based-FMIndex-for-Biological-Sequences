975f352cae435d3270417e0bb53a1afe8f36a6da
kacso <dsokac1@gmail.com>
2014-12-31 14:10:54 +0100

    Resloved problem with last char from alphabet in FMIndex

--- src/FMIndex.cpp
+++ src/FMIndex.cpp
@@ -16,7 +16,7 @@ int FMIndex::count(std::string substring){
 	count(start, end, substring);
 	cnt = end - start + 1;
 	if (cnt < 0)
-		return -1;
+		return 0;
 	else
 		return cnt;
 }
@@ -24,12 +24,28 @@ int FMIndex::count(std::string substring){
 void FMIndex::count(int &start, int &end, std::string substring){
 	int index = substring.length() - 1;
 	char nextChar = substring[index--];
-	start = lfTable->countLast(nextChar);  //+1
-	
-	end = lfTable->countLast(alphabet[mapAlphabet[nextChar] + 1]) - 1;
 
-	while (index >= 0){
+	if (mapAlphabet.count(nextChar) == 0){
+		start = -1;
+		end = -2;
+		return;
+	}
+	
+	start = lfTable->countLast(nextChar);  //+1
+
+	if (mapAlphabet[nextChar] == mapAlphabet.size() - 1){
+		end = lfTable->getLengthLast() - 1;
+	}
+	else{
+		end = lfTable->countLast(alphabet[mapAlphabet[nextChar] + 1]) - 1;
+	}
+	while (index >= 0 && start <= end){
 		nextChar = substring[index--];
+		if (mapAlphabet.count(nextChar) == 0){
+			start = -1;
+			end = -2;
+			return;
+		}
 		start = lfTable->countLast(nextChar) + lfTable->getRankLast(nextChar, start - 1);
 		end = lfTable->countLast(nextChar) + lfTable->getRankLast(nextChar, end) - 1;
 	}
